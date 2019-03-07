f4d1584a45a8680d02a676f1aae15edf5d4b3692
Fabian Hutzler <u22q42@atgrzsl3213.avl01.avlcorp.lan>
2019-03-07 09:25:55 +0100

    first commit

---
+++ build/precache-manifest.9364129bec5e61584a0192892107a896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62c7b600941ad4493d0e",
+    "url": "/static/css/main.d448ef82.chunk.css"
+  },
+  {
+    "revision": "62c7b600941ad4493d0e",
+    "url": "/static/js/main.62c7b600.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a1970b7a7edc7fdf85e5ab7154a260ea",
+    "url": "/index.html"
+  }
+];
