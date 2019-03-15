6c9023a4546ad7490d976b5cd1604a195273dd45
tabidax <u22q42@atgrzsl3213.avl01.avlcorp.lan>
2019-03-15 13:55:53 +0100

    changed to benny

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-                Hi my name is Konrad
+                Hi my name is Benny
           </p>
           <a
             className="App-link"
